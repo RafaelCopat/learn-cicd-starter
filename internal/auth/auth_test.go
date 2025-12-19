8d4effe7c83bf2376d69f7b75c383199ff927155
Rafael Copat <rafael.copat@sap.com>
2025-12-19 15:51:53 -0300

    fix

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -19,18 +19,3 @@ func TestGetAPIKey_Success(t *testing.T) {
 		t.Fatalf("expected key %q, got %q", "12345", gotKey)
 	}
 }
-
-func TestGetAPIKey_Success2(t *testing.T) {
-	headers := http.Header{
-		"Authorization": {"ApiKey 12345"},
-	}
-
-	gotKey, err := GetAPIKey(headers)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	if gotKey != "12345" {
-		t.Fatalf("expected key %q, got %q", "12345", gotKey)
-	}
-}
